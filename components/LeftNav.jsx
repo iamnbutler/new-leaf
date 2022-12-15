922c44f65602eb051a4eefcd6bb861be803a3fea
Nate Butler <iamnbutler@gmail.com>
2022-12-14 21:21:12 -0500

    Add footer links

--- components/LeftNav.jsx
+++ components/LeftNav.jsx
@@ -1,6 +1,17 @@
 import NavLink from "./NavLink";
 import { allPosts } from "contentlayer/generated"
 
+const metaNav = [
+  {
+    title: "View on Github",
+    url: "https://github.com/iamnbutler/new-leaf"
+  },
+  {
+    title: "Pull Requests",
+    url: "https://github.com/iamnbutler/new-leaf/pulls"
+  },
+]
+
 export default function LeftNav() {
   return (
     <nav
@@ -15,6 +26,18 @@ export default function LeftNav() {
           />
         ))}
       </div>
+      <menu className="py-4 space-y-px flex flex-col">
+        {metaNav.map((item) => (
+          <a
+            href={item.url}
+            key={item.url}
+            className="space-y-[6px] max-w-[360px] px-3 mx-2 py-2 rounded font-mono
+            bg-transparent hover:bg-surface text-sm text-secondary"
+          >
+            {item.title}
+          </a>
+        ))}
+      </menu>
     </nav>
   );
 }
