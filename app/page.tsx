682c61db1d97d03ea43f79dfbf45060ad1a8b00d
Nate Butler <iamnbutler@gmail.com>
2022-12-14 22:37:24 -0500

    Render markdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-export default function HomePage() {
+import { allPosts, Post } from "contentlayer/generated"
+import ReactMarkdown from "react-markdown"
+
+function Article({ post }: { post: Post }) {
   return (
-    <h1>Home Page</h1>
+    <article>
+      <h2>{post.title}</h2>
+      <time>{post.date_created}</time>
+      <ReactMarkdown>
+        {post.body.raw}
+      </ReactMarkdown>
+    </article>
+  )
+}
+
+export default function HomePage() {
+  const posts = allPosts
+
+  return (
+    <main>
+      <h1>Home Page</h1>
+      {posts.map(post => (
+        <Article post={post} key={post.uuid} />
+      ))}
+    </main>
   )
 }
